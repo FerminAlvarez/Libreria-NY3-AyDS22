5d01c488e9d1e47b489d850eb6066c64cab64fc5
NicolasLeidi <71295543+NicolasLeidi@users.noreply.github.com>
2022-04-20 10:17:00 -0300

    Logs de testing eliminados, TODO's eliminados

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -23,21 +23,19 @@ class OtherInfoWindow : AppCompatActivity() {
     private val logoNYT =
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
     private var NYTInfoPane: TextView? = null
-    private var dataBase: DataBase? = null
+    private lateinit var dataBase: DataBase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other_info)
 
         NYTInfoPane = findViewById(R.id.NYTInfoPane)
+        //TODO Checkear null
         open(intent.getStringExtra("artistName"))
     }
 
     private fun open(artist: String?) {
         dataBase = DataBase(this)
-        DataBase.saveArtist(dataBase!!, "test", "sarasa")
-        Log.e("TAG", "" + DataBase.getInfo(dataBase!!, "test"))
-        Log.e("TAG", "" + DataBase.getInfo(dataBase!!, "nada"))
         getArtistInfo(artist!!)
     }
 
@@ -50,16 +48,13 @@ class OtherInfoWindow : AppCompatActivity() {
             .build()
         val NYTimesAPI = retrofit.create(NYTimesAPI::class.java)
 
-        //TODO ¿Otro log?
-        Log.e("TAG", "artistName $artistName")
-
         //TODO colocar un nombre representativo, sacarle los argumentos posibles
         CreateThreadCAMBIARNOMBRE(artistName, NYTimesAPI)
     }
 
     private fun CreateThreadCAMBIARNOMBRE(artistName: String, NYTimesAPI: NYTimesAPI) {
         Thread {
-            val infoDB = DataBase.getInfo(dataBase!!, artistName)
+            val infoDB = DataBase.getInfo(dataBase, artistName)
             var NYTinfo = ""
             //TODO sacar comentarios, cambiar el != null por algo de null operators
             if (infoDB != null) { // exists in db
@@ -70,21 +65,18 @@ class OtherInfoWindow : AppCompatActivity() {
                     callResponse = NYTimesAPI.getArtistInfo(artistName).execute()
                     Log.e("TAG", "JSON " + callResponse.body())
                     val gson = Gson()
-                    //TODO cambiar jobj por algo representativo
                     val jobj = gson.fromJson(callResponse.body(), JsonObject::class.java)
                     val response = jobj["response"].asJsonObject
-                    //TODO cambiar _abstract porque incluso el nombre es una palabra reservada
                     val abstractNYT = response["docs"].asJsonArray[0].asJsonObject["abstract"]
                     val urlNYT = response["docs"].asJsonArray[0].asJsonObject["web_url"]
 
-                    //TODO Este text tendría que ser otra variable, no tiene que ver con lo que hace la anterior
                     if (abstractNYT == null) {
                         NYTinfo = "No Results"
                     } else {
                         NYTinfo = abstractNYT.asString.replace("\\n", "\n")
                         NYTinfo = textToHtml(NYTinfo, artistName)
                         // save to DB  <o/
-                        DataBase.saveArtist(dataBase!!, artistName, NYTinfo)
+                        DataBase.saveArtist(dataBase, artistName, NYTinfo)
                     }
                     val urlString = urlNYT.asString
                     findViewById<View>(R.id.openUrlButton).setOnClickListener {
@@ -93,14 +85,11 @@ class OtherInfoWindow : AppCompatActivity() {
                         this.startActivity(intent)
                     }
                 } catch (e1: IOException) {
-                    //TODO más logs
                     Log.e("TAG", "Error $e1")
                     e1.printStackTrace()
                 }
             }
 
-            //TODO Hay un log
-            Log.e("TAG", "Get Image from $logoNYT")
             val finalText = NYTinfo
             runOnUiThread {
                 Picasso.get().load(logoNYT).into(findViewById<View>(R.id.imageView) as ImageView)
@@ -123,7 +112,6 @@ class OtherInfoWindow : AppCompatActivity() {
     }
 
     companion object {
-        //TODO ("Esta constante la usa la clase HomeView de forma estatica")
         const val ARTIST_NAME_EXTRA = "artistName"
     }
 }
