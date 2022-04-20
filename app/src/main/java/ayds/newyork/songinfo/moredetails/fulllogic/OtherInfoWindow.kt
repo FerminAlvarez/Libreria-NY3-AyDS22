706db375b09b463ac7b75ec12436cda64a526901
NicolasLeidi <71295543+NicolasLeidi@users.noreply.github.com>
2022-04-20 11:50:51 -0300

    Refactor inline, extraer funcion, renombres y mover variables

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -11,71 +11,71 @@ import com.google.gson.JsonObject
 import android.content.Intent
 import android.net.Uri
 import com.squareup.picasso.Picasso
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.core.text.HtmlCompat
 import com.google.gson.JsonElement
-import retrofit2.Response
-import java.io.IOException
 import java.lang.StringBuilder
 
 class OtherInfoWindow : AppCompatActivity() {
     private val logoNYT =
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
-    private var NYTInfoPane: TextView? = null
+    private lateinit var nytInfoPane: TextView
     private lateinit var dataBase: DataBase
+    private lateinit var nytimesAPI: NYTimesAPI
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other_info)
 
-        NYTInfoPane = findViewById(R.id.NYTInfoPane)
+        nytInfoPane = findViewById(R.id.NYTInfoPane)
 
         openDataBase()
-        //TODO colocar un nombre representativo, sacarle los argumentos posibles
-        CreateThreadCAMBIARNOMBRE(intent.getStringExtra("artistName")!!)
+        //TODO checkear null
+        prepareArtistInfoView(intent.getStringExtra("artistName")!!)
     }
 
     private fun openDataBase() {
         dataBase = DataBase(this)
     }
 
-    private fun CreateThreadCAMBIARNOMBRE(artistName: String) {
+    private fun prepareArtistInfoView(artistName: String) {
         Thread {
             val infoDB = DataBase.getInfo(dataBase, artistName)
-            var NYTinfo = ""
+            val artistInfo : String
             //TODO sacar comentarios, cambiar el != null por algo de null operators
             if (infoDB != null) { // exists in db
-                NYTinfo = "[*]$infoDB"
+                artistInfo = "[*]$infoDB"
             } else { // get from service
-                try {
-                    val NYTimesAPI = createRetrofit()
-                    val response = createArtistInfoJsonObject(NYTimesAPI, artistName)
-                    val abstractNYT = response["docs"].asJsonArray[0].asJsonObject["abstract"]
-
-                    if (abstractNYT == null) {
-                        NYTinfo = "No Results"
-                    } else {
-                        NYTinfo = abstractNYT.asString.replace("\\n", "\n")
-                        NYTinfo = artistNameToHtml(NYTinfo, artistName)
-
-                        DataBase.saveArtist(dataBase, artistName, NYTinfo)
-                    }
-
-                    val urlNYT = response["docs"].asJsonArray[0].asJsonObject["web_url"]
-                    createURLButtonListener(urlNYT)
-                } catch (e1: IOException) {
-                    e1.printStackTrace()
-                }
+                createRetrofit()
+                val response = createArtistInfoJsonObject(artistName)
+                val abstractNYT = response["docs"].asJsonArray[0].asJsonObject["abstract"]
+                artistInfo = getArtistInfo(abstractNYT, artistName)
+                abstractNYT.let { DataBase.saveArtist(dataBase, artistName, artistInfo) }
+                val urlNYT = response["docs"].asJsonArray[0].asJsonObject["web_url"]
+                createURLButtonListener(urlNYT)
             }
 
-            val finalText = NYTinfo
             runOnUiThread {
                 Picasso.get().load(logoNYT).into(findViewById<View>(R.id.imageView) as ImageView)
-                NYTInfoPane!!.text = HtmlCompat.fromHtml(finalText, HtmlCompat.FROM_HTML_MODE_LEGACY)
+                nytInfoPane.text =
+                    HtmlCompat.fromHtml(artistInfo, HtmlCompat.FROM_HTML_MODE_LEGACY)
             }
         }.start()
+    }
+
+    private fun getArtistInfo(
+        abstractNYT: JsonElement?,
+        artistName: String
+    ): String {
+        var artistInfoResult : String
+        if (abstractNYT == null) {
+            artistInfoResult = "No Results"
+        } else {
+            artistInfoResult = abstractNYT.asString.replace("\\n", "\n")
+            artistInfoResult = artistNameToHtml(artistInfoResult, artistName)
+        }
+        return artistInfoResult
     }
 
     private fun createRetrofit(): NYTimesAPI {
@@ -83,7 +83,7 @@ class OtherInfoWindow : AppCompatActivity() {
             .baseUrl("https://api.nytimes.com/svc/search/v2/")
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
-        return retrofit.create(NYTimesAPI::class.java)
+        return retrofit.create(nytimesAPI::class.java)
     }
 
     private fun createURLButtonListener(urlNYT: JsonElement) {
@@ -96,14 +96,12 @@ class OtherInfoWindow : AppCompatActivity() {
     }
 
     private fun createArtistInfoJsonObject(
-        NYTimesAPI: NYTimesAPI,
         artistName: String
     ): JsonObject {
-        val callResponse = NYTimesAPI.getArtistInfo(artistName).execute()
+        val callResponse = nytimesAPI.getArtistInfo(artistName).execute()
         val gson = Gson()
         val jobj = gson.fromJson(callResponse.body(), JsonObject::class.java)
-        val response = jobj["response"].asJsonObject
-        return response
+        return jobj["response"].asJsonObject
     }
 
     private fun artistNameToHtml(NYTinfo: String, artistName: String): String {
