3455a23eea779966fd392b5ddae660ad23b05740
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-04-22 16:16:51 -0300

    Refactor - Eliminado el companion object

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
@@ -13,11 +13,13 @@ private const val ARTISTS_TABLE = "artists"
 private const val ID_COLUMN = "id"
 private const val ARTIST_COLUMN = "artist"
 private const val INFO_COLUMN = "info"
-private const val CREATE_TABLE_QUERY = "create table $ARTISTS_TABLE ($ID_COLUMN INTEGER PRIMARY KEY AUTOINCREMENT, $ARTIST_COLUMN string, $INFO_COLUMN string)"
+private const val CREATE_TABLE_QUERY =
+    "create table $ARTISTS_TABLE ($ID_COLUMN INTEGER PRIMARY KEY AUTOINCREMENT, $ARTIST_COLUMN string, $INFO_COLUMN string)"
 private const val QUERY_SELECTION = "$ARTIST_COLUMN  = ?"
 private const val QUERY_SORT_ORDER = "$ARTIST_COLUMN DESC"
 
-class DataBase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME , null, DATABASE_VERSION) {
+class DataBase(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(database: SQLiteDatabase) {
         createArtistsTable(database)
     }
@@ -26,60 +28,56 @@ class DataBase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME , nul
 
     override fun onUpgrade(database: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
 
-    companion object {
-
-        @JvmStatic
-        fun saveArtist(dbHelper: DataBase, artist: String, info: String) {
-            val database = dbHelper.writableDatabase
-            val newArtist = createArtist(artist, info)
-            insertNewArtist(database, newArtist)
-        }
-
-        private fun createArtist(artist: String, info: String): ContentValues {
-            val values = ContentValues()
-            values.put(ARTIST_COLUMN, artist)
-            values.put(INFO_COLUMN, info)
-            return values
-        }
-
-        private fun insertNewArtist(database: SQLiteDatabase, values: ContentValues): Long =
-            database.insert(ARTISTS_TABLE, null, values)
-
-        @JvmStatic
-        fun getInfo(dbHelper: DataBase, artist: String): String? {
-            val database = dbHelper.readableDatabase
-            val cursor = getCursor(database, artist)
-            val items = getArtistInfo(cursor)
-            closeCursor(cursor)
-            return if (items.isEmpty()) null else items[0]
-        }
-
-        private fun getCursor(database: SQLiteDatabase, artist: String): Cursor {
-            val projection = arrayOf(ID_COLUMN, ARTIST_COLUMN, INFO_COLUMN)
-            val selectionArgs = arrayOf(artist)
-            return database.query(
-                ARTISTS_TABLE,
-                projection,
-                QUERY_SELECTION,
-                selectionArgs,
-                null,
-                null,
-                QUERY_SORT_ORDER
-            )
-        }
-
-        private fun getArtistInfo(cursor: Cursor): MutableList<String> {
-            val items: MutableList<String> = ArrayList()
-            while (cursor.moveToNext()) {
-                val info = cursor.getString(
-                    cursor.getColumnIndexOrThrow(INFO_COLUMN)
-                )
-                items.add(info)
-            }
-            return items
-        }
-
-        private fun closeCursor(cursor: Cursor) =
-            cursor.close()
+    fun saveArtist(artist: String, info: String) {
+        val newArtist = createArtist(artist, info)
+        insertNewArtist(newArtist)
     }
+
+    private fun createArtist(artist: String, info: String): ContentValues {
+        val values = ContentValues()
+        values.put(ARTIST_COLUMN, artist)
+        values.put(INFO_COLUMN, info)
+        return values
+    }
+
+    private fun insertNewArtist(artist: ContentValues): Long {
+        val database = this.writableDatabase
+        return database.insert(ARTISTS_TABLE, null, artist)
+    }
+
+    fun getInfo(artist: String): String? {
+        val cursor = getCursor(artist)
+        val items = getArtistInfo(cursor)
+        closeCursor(cursor)
+        return if (items.isEmpty()) null else items[0]
+    }
+
+    private fun getCursor(artist: String): Cursor {
+        val database = this.readableDatabase
+        val projection = arrayOf(ID_COLUMN, ARTIST_COLUMN, INFO_COLUMN)
+        val selectionArgs = arrayOf(artist)
+        return database.query(
+            ARTISTS_TABLE,
+            projection,
+            QUERY_SELECTION,
+            selectionArgs,
+            null,
+            null,
+            QUERY_SORT_ORDER
+        )
+    }
+
+    private fun getArtistInfo(cursor: Cursor): MutableList<String> {
+        val items: MutableList<String> = ArrayList()
+        while (cursor.moveToNext()) {
+            val info = cursor.getString(
+                cursor.getColumnIndexOrThrow(INFO_COLUMN)
+            )
+            items.add(info)
+        }
+        return items
+    }
+
+    private fun closeCursor(cursor: Cursor) =
+        cursor.close()
 }
