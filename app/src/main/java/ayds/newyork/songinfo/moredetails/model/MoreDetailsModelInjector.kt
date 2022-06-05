0bc387b5d05f3901f4610733f07b4e823a8c6865
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-06-05 17:52:11 -0300

    SubModule - refactor simplificando el proxyList

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/MoreDetailsModelInjector.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/MoreDetailsModelInjector.kt
@@ -4,7 +4,6 @@ import ayds.newyork.songinfo.moredetails.model.repository.broker.proxy.lastfm.La
 import ayds.newyork.songinfo.moredetails.model.repository.broker.proxy.wikipedia.WikipediaProxyImpl
 import android.content.Context
 import ayds.lisboa.lastfmdata.lastfm.LastFMInjector
-import ayds.lisboa.lastfmdata.lastfm.LastFMService
 import ayds.newyork.songinfo.moredetails.model.repository.ArtistInfoRepository
 import ayds.newyork.songinfo.moredetails.model.repository.ArtistInfoRepositoryImpl
 import ayds.newyork.songinfo.moredetails.model.repository.broker.InfoBroker
@@ -15,11 +14,9 @@ import ayds.newyork.songinfo.moredetails.model.repository.local.card.LocalStorag
 import ayds.newyork.songinfo.moredetails.model.repository.local.card.sqldb.CursorToArtistArticleMapperImpl
 import ayds.newyork.songinfo.moredetails.model.repository.local.card.sqldb.LocalStorageImpl
 import ayds.newyork.songinfo.moredetails.view.MoreDetailsView
-import ayds.ny3.newyorktimes.NytArticleService
 import ayds.ny3.newyorktimes.NytInjector
-import ayds.winchester1.wikipedia.WikipediaCardService
 import ayds.winchester1.wikipedia.WikipediaInjector
-import java.util.*
+import kotlin.collections.ArrayList
 
 object MoreDetailsModelInjector {
 
@@ -31,20 +28,12 @@ object MoreDetailsModelInjector {
         val localStorage: LocalStorage = LocalStorageImpl(
             moreDetailsView as Context, CursorToArtistArticleMapperImpl()
         )
-        val proxyList = LinkedList<ServiceProxy>()
 
-        val nytArticleService: NytArticleService = NytInjector.nytArticleService
-        val nytProxy: ServiceProxy = NytProxyImpl(nytArticleService)
-
-        val lastFMArticleService: LastFMService = LastFMInjector.lastFMService
-        val lastFMProxy: ServiceProxy = LastFMProxyImpl(lastFMArticleService)
-
-        val wikipediaArticleService: WikipediaCardService = WikipediaInjector.wikipediaCardService
-        val wikipediaProxy: ServiceProxy = WikipediaProxyImpl(wikipediaArticleService)
-
-        proxyList.add(nytProxy)
-        proxyList.add(lastFMProxy)
-        proxyList.add(wikipediaProxy)
+        val proxyList: ArrayList<ServiceProxy> = arrayListOf(
+            NytProxyImpl(NytInjector.nytArticleService),
+            LastFMProxyImpl(LastFMInjector.lastFMService),
+            WikipediaProxyImpl(WikipediaInjector.wikipediaCardService),
+        )
 
         val infoBroker: InfoBroker = InfoBrokerImpl(proxyList)
 
